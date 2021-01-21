deabc1aff7def379eef4fdfed327704b9035e0fc
JorPan <jbpanasewicz@gmail.com>
2021-01-21 11:10:14 -0700

    frontend

---
+++ index.js
@@ -0,0 +1,132 @@
+const APIKey = "4nX8ujH";
+const searchParams = new URLSearchParams(window.location.search);
+const search = searchParams.get("search");
+
+let positiveEffects = [];
+let medicalEffects = [];
+let negativeEffects = [];
+let effectsURL = `http://strainapi.evanbusse.com/${APIKey}/searchdata/effects`;
+
+// if (search) {
+//   effectsurl = `http://localhost:3001/strains?search=${search}`;
+//   fetch(effectsURL).then(((response) => response.json()).then(console.log));
+// }
+
+fetch(effectsURL)
+  .then((response) => response.json())
+  .then((effects) => {
+    filterPositiveEffects(effects);
+    filterMedicalEffects(effects);
+    filterNegativeEffects(effects);
+    showPositiveEffects(positiveEffects);
+    showMedicalEffects(medicalEffects);
+    showNegativeEffects(negativeEffects);
+  });
+
+function filterPositiveEffects(effects) {
+  effects.forEach((effect) => {
+    if (effect.type == "positive") {
+      positiveEffects.push(effect);
+    }
+  });
+}
+
+function showPositiveEffects(positiveEffects) {
+  let positiveDiv = document.querySelector("#positive-div");
+  let effectsection = document.querySelector("#fieldset");
+
+  positiveEffects.forEach((effect) => {
+    let option = document.createElement("input");
+    let label = document.createElement("label");
+    let linebreak = document.createElement("br");
+    option.classList.add("selectableEffect");
+    option.type = "checkbox";
+    label = effect["effect"];
+    option.setAttribute("name", `${label}-effect-positive`);
+    option.id = label;
+    // label.for = label;
+    positiveDiv.append(option);
+    positiveDiv.append(label);
+    positiveDiv.append(linebreak);
+  });
+  effectsection.append(positiveDiv);
+}
+
+function filterMedicalEffects(effects) {
+  effects.forEach((effect) => {
+    if (effect.type == "medical") {
+      medicalEffects.push(effect);
+    }
+  });
+}
+
+function showMedicalEffects(medicalEffects) {
+  let medicalDiv = document.querySelector("#medical-div");
+  let effectsection = document.querySelector("#fieldset");
+
+  medicalEffects.forEach((effect) => {
+    let option = document.createElement("input");
+    let label = document.createElement("label");
+    let linebreak = document.createElement("br");
+    option.classList.add("selectableEffect");
+    option.type = "checkbox";
+    label = effect["effect"];
+    option.setAttribute("name", `${label}-effect-medical`);
+    option.id = label;
+    medicalDiv.append(option);
+    medicalDiv.append(label);
+    medicalDiv.append(linebreak);
+  });
+  effectsection.append(medicalDiv);
+}
+
+function filterNegativeEffects(effects) {
+  effects.forEach((effect) => {
+    if (effect.type == "negative") {
+      negativeEffects.push(effect);
+    }
+  });
+}
+
+function showNegativeEffects(negativeEffects) {
+  let negativeDiv = document.querySelector("#negative-div");
+  let effectsection = document.querySelector("#fieldset");
+
+  negativeEffects.forEach((effect) => {
+    let option = document.createElement("input");
+    let label = document.createElement("label");
+    let linebreak = document.createElement("br");
+    option.classList.add("selectableEffect");
+    option.type = "checkbox";
+    label = effect["effect"];
+    option.setAttribute("name", `${label}-effect-negative`);
+    option.id = label;
+    negativeDiv.append(option);
+    negativeDiv.append(label);
+    negativeDiv.append(linebreak);
+  });
+  effectsection.append(negativeDiv);
+}
+
+const params = new URLSearchParams(window.location.search);
+let mySearchParams = params.get("strains");
+
+let effectForm = document.querySelector("#effects-form");
+effectForm.addEventListener("submit", getCheckedOptions);
+
+function getCheckedOptions(event) {
+  checkedOptions = document.querySelectorAll("input[type=checkbox]:checked");
+  getStrings(checkedOptions);
+}
+
+let desiredEffects = [];
+
+function getStrings(checkedOptions) {
+  return Array.from(checkedOptions).map((element) => {
+    desiredEffects.push(element.name);
+  });
+}
+
+// fetch("http://localhost:3000/joiners")
+//   .then((response) => response.json())
+//   .then(console.log);
---
+++ suggestions.js
@@ -0,0 +1,39 @@
+// fetch("http://localhost:3000/joiners")
+fetch("https://herbalstrains.herokuapp.com/joiners")
+  .then((response) => response.json())
+  .then((strains) => {
+    strains.forEach((strain) => {
+      let section = document.querySelector("#strain-cards");
+      let card = document.createElement("div");
+      let name = document.createElement("h2");
+      let category = document.createElement("h3");
+      let cardHead = document.createElement("div");
+      let flavors = document.createElement("h4");
+      let positive_effects = document.createElement("p");
+      let medical_effects = document.createElement("p");
+      let negative_effects = document.createElement("p");
+      cardHead.classList.add("card-head");
+
+      card.classList.add("strain-card");
+      name.textContent = strain.name;
+      category.textContent = strain.category.toUpperCase();
+      flavors.textContent = "Flavors: " + JSON.parse(strain.flavors);
+      positive_effects.textContent =
+        "Positive Effects: " + JSON.parse(strain.positive_effects);
+      medical_effects.textContent =
+        "Medical Effects: " + JSON.parse(strain.medical_effects);
+      negative_effects =
+        "Negative Effects: " + JSON.parse(strain.negative_effects);
+
+      cardHead.append(name, category);
+
+      card.append(
+        cardHead,
+        flavors,
+        positive_effects,
+        medical_effects,
+        negative_effects
+      );
+      section.append(card);
+    });
+  });
